57aad9f1571248cad7ce4c2de9c1e2752b6d1b7c
Krish Soni <isonikrish@gmail.com>
2025-10-24 13:00:58 +0530

    feat: project setup and db setup

---
+++ db/schema.ts
@@ -0,0 +1,26 @@
+import { pgTable, text, integer, serial, boolean, timestamp } from "drizzle-orm/pg-core";
+
+export const blogsTable = pgTable("blogs", {
+  id: serial("id").primaryKey(),
+  title: text("title").notNull(),
+  content: text("content").notNull(),
+  slug: text("slug").notNull().unique(),
+  imageUrl: text("imageUrl"),
+  published: boolean("published").default(false),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").defaultNow(),
+});
+
+export const categoriesTable = pgTable("categories", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  description: text("description"),
+  slug: text("slug").notNull().unique(),
+});
+
+export const postCategoriesTable = pgTable("post_categories", {
+  id: serial("id").primaryKey(),
+  blogId: integer("blog_id").notNull().references(() => blogsTable.id, { onDelete: "cascade" }),
+  categoryId: integer("category_id").notNull().references(() => categoriesTable.id, { onDelete: "cascade" }),
+});
+ 
